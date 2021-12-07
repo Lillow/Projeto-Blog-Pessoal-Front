7c28f9449c854d7645f311c30b58fcfed8118583
Danillo Silva <danillordm@gmail.com>
2021-12-07 12:46:05 -0300

    @Lillow [cadastrar integration]

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -13,7 +13,7 @@ export class AuthService {
   entrar(userLogin: UserLogin): Observable<UserLogin> {
     return this.http.post<UserLogin>(
       'http://localhost:8080/usuarios/logar',
-      UserLogin
+      userLogin
     );
   }
 
