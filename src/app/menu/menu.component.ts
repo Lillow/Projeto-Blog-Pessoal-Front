f695174f31c7df669607ab4c7cb3d8c950862b66
Danillo Silva <danillordm@gmail.com>
2021-12-08 21:31:19 -0300

    @Lillow [inicio component and other adjustments]

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent implements OnInit {
+  nome = environment.nome;
+  foto = environment.foto;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit() {}
+
+  sair() {
+    this.router.navigate(['/entrar']);
+    environment.token = '';
+    environment.nome = '';
+    environment.foto = '';
+    environment.id = 0;
   }
-
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
@@ -22,5 +23,15 @@ export class AuthService {
       'https://blog-pessoal-lillow.herokuapp.com/usuarios/cadastrar',
       user
     );
+  }
+
+  logado() {
+    let ok = false;
+
+    if (environment.token != '') {
+      ok = true;
+    }
+
+    return ok;
   }
 }
