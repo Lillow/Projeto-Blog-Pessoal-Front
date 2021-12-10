a2bc1391232309ae3dc7619826a8c8f8ff991baa
Danillo Silva <danillordm@gmail.com>
2021-12-10 18:42:31 -0300

    @Lillow [Implementation of get and post temas]

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -8,11 +10,32 @@ import { environment } from 'src/environments/environment.prod';
   styleUrls: ['./tema.component.css'],
 })
 export class TemaComponent implements OnInit {
-  constructor(private router: Router) {}
+  tema: Tema = new Tema();
+  listaTemas: Tema[];
 
-  ngOnInit(): void {
+  constructor(private router: Router, private temaService: TemaService) {}
+
+  ngOnInit() {
     if (environment.token == '') {
       this.router.navigate(['/entrar']);
     }
+    this.temaService.refreshToken();
+
+    this.findAllTemas();
+  }
+
+  findAllTemas() {
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp;
+    });
+  }
+
+  cadastrar() {
+    this.temaService.postTema(this.tema).subscribe((resp: Tema) => {
+      this.tema = resp;
+      alert('Tema cadastrado com sucesso!');
+      this.tema = new Tema();
+      this.findAllTemas();
+    });
   }
 }
