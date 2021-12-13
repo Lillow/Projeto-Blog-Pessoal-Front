741fd9107df3d4e9c0e1d7cad94201b0ba668eba
Danillo Silva <danillordm@gmail.com>
2021-12-13 11:36:15 -0300

    @Lillow [implementation put and delete temas]

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -27,10 +27,32 @@ export class TemaService {
     );
   }
 
+  getByIdTema(id: number): Observable<Tema> {
+    return this.http.get<Tema>(
+      `https://blog-pessoal-lillow.herokuapp.com/tema/${id}`,
+      this.token
+    );
+  }
+
   postTema(tema: Tema): Observable<Tema> {
     return this.http.post<Tema>(
       'https://blog-pessoal-lillow.herokuapp.com/tema',
       tema,
+      this.token
+    );
+  }
+
+  putTema(tema: Tema): Observable<Tema> {
+    return this.http.put<Tema>(
+      'https://blog-pessoal-lillow.herokuapp.com/tema',
+      tema,
+      this.token
+    );
+  }
+
+  deleteTema(id: number) {
+    return this.http.delete(
+      `https://blog-pessoal-lillow.herokuapp.com/tema/${id}`,
       this.token
     );
   }
